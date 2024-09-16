593e71e2aaaa37bd4b46d7567c78fa6992acd69f
David Alberto Adler <dalberto.adler@gmail.com>
2024-09-16 15:44:03 +0100

    remove text based browser `www-browser`

--- browser_linux.go
+++ browser_linux.go
@@ -6,7 +6,7 @@ import (
 )
 
 func openBrowser(url string) error {
-	providers := []string{"xdg-open", "x-www-browser", "www-browser"}
+	providers := []string{"xdg-open", "x-www-browser"}
 
 	// There are multiple possible providers to open a browser on linux
 	// One of them is xdg-open, another is x-www-browser, then there's www-browser, etc.
